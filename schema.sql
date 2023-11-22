f1598f536aa89db5b30df56fa803ccf4ec488bc8
lilian <lilia@2a02-8440-c113-ea27-defb-48ff-fead-1a79.rev.sfr.net>
2023-11-22 16:09:44 +0100

    tableSQL

---
+++ schema.sql
@@ -0,0 +1,13 @@
+-- Fichier schema.sql
+
+-- Supprime la table si elle existe déjà
+DROP TABLE IF EXISTS utilisateurs;
+
+-- Crée une nouvelle table utilisateurs
+CREATE TABLE utilisateurs (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom TEXT NOT NULL,
+    email TEXT NOT NULL,
+    mot_de_passe TEXT NOT NULL
+);
+
